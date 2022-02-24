a9e170365ede086611e28e3789cf277540cfafce
Guille Fernandez <guillermofernandez16@gmail.com>
2022-02-24 17:35:41 -0300

    Agregar archivos de proyecto.

---
+++ Proyecto 55/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_55
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int suma, valor;
+            string linea;
+            suma = 0;
+
+            do
+            {
+                Console.Write("Ingrese un valor (Para terminar ingrese 9999): ");
+                linea = Console.ReadLine();
+                valor = int.Parse(linea);
+
+                if (valor != 9999)
+                {
+                   
+                    suma = valor + suma;
+                }
+               
+
+            } while (valor != 9999);
+
+            if (suma != 0)
+            {
+                if (suma > 0)
+                {
+                    Console.Write("La suma de los valores es: ");
+                    Console.WriteLine(suma);
+                    Console.Write("El valor de la suma es mayor a 0");
+                }
+                else
+                {
+                    Console.Write("La suma de los valores es: ");
+                    Console.WriteLine(suma);
+                    Console.Write("El valor de la suma es menor a 0");
+                }
+            }
+            else
+            {
+                Console.Write("La suma de los valores es 0");
+            }
+            Console.ReadKey();
+        }
+    }
+}
